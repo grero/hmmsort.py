0a6fe6e0f7e2223ba8171288ec02d9f23f071c76
Roger Herikstad <roger.herikstad@gmail.com>
2013-06-12 15:38:39 +0800

    Made several changes to utliity to make sure the numba accelerated code does the same thing as the original code

--- utility.py
+++ utility.py
@@ -4,6 +4,20 @@ import sys
 import tempfile
 import time
 import pylab as plt
+import blosc
+import os
+
+@numba.autojit
+def testfunc(a):
+    a[:] = a[::-1] 
+
+@numba.autojit(locals={'n': numba.int_})
+def testfunc2(a):
+    n = a.shape[0]
+    for i in xrange(a.shape[0]):
+        q = a[i]
+        a[i] = a[n-i-1] 
+        a[n-i-1]  = q
 
 @numba.autojit
 def forward(data, W, g, spklength, 
@@ -12,8 +26,11 @@ def forward(data, W, g, spklength,
     P = p.sum()
     for t in xrange(1, winlength):
         f[:] = W - data[t]
-        #f = np.exp(-0.5*(y*np.dot(c, y)).sum(0)) + tiny
-        f[:] = np.exp(-0.5*(f*f*c)) + tiny
+        #ff = 0.
+       # for k in xrange(W.shape[0]):
+       #     ff += f[k]*f[k]
+        #ff = np.exp(-0.5*ff*c) + tiny
+        f[:] = np.exp(-0.5*f*f*c) + tiny
         g[:, t] = g[q, t - 1]
         gg = g[1:2 + (N - 1)*(spklength - 1):(spklength - 1), t]
         a = 0.
@@ -21,7 +38,7 @@ def forward(data, W, g, spklength,
             a += gg[k] 
         g[0, t] = a + g[0, t] - g[0, t - 1]*P
         g[1:2 + (N - 1) * (spklength - 1):(spklength - 1), t] = g[0, t - 1] * p
-        g[:, t] = g[:, t]*f[:] + tiny
+        g[:, t] = g[:, t]*f + tiny
         a = 0.
         for k in xrange(g.shape[0]):
             a += g[k, t]
@@ -32,36 +49,76 @@ def forward2(data, W, g, spklength,
     tiny = np.exp(-700)
     for t in xrange(1, winlength):
         f[:] = W-data[t]
+        ff = 0.
+        for k in xrange(W.shape[0]):
+            ff += f[k]*f[k]
+        ff = np.exp(-0.5*ff*c) + tiny
         #f = np.exp(-0.5*(y*np.dot(c, y)).sum(0)) + tiny
-        f[:] = np.exp(-0.5*(f*f*c)) + tiny
         g[:, t] = g[q, t - 1]
         g[0, t] = (g[1:2 + (N - 1)*(spklength - 1):(spklength - 1), t].sum() + 
                     g[0, t] - g[0, t - 1]*p.sum())
         g[1:2 + (N - 1) * (spklength - 1):(spklength - 1), t] = g[0, t - 1] * p
-        g[:, t] = g[:, t]*f[:] + tiny
+        g[:, t] = g[:, t]*ff + tiny
         g[:, t] = g[:, t] / (g[:, t].sum()+tiny)
 
-@numba.autojit
+@numba.autojit(locals={'P': numba.float64,
+                       'a': numba.float64,
+                       's': numba.float64,
+                       'gg': numba.float64[:],
+                       'bb': numba.float64[:]})
 def backward(data, W, g, spklength, 
+            winlength, c, q, N, p, b,f):
+    tiny = np.exp(-700)
+    P = p.sum()
+    bb = np.zeros(b.shape)
+    nb = b.shape[0]
+    for t in xrange(winlength - 2, -1, -1):
+        f[:] = W - data[t + 1]
+        f[:] = np.exp(-0.5*f*f*c) + tiny
+        #b[:] = b*f + tiny
+        for i in xrange(b.shape[0]):
+            b[i] = b[i]*f[i] + tiny
+        #rearrange
+        for i in xrange(b.shape[0]):
+            bb[q[i]] = b[i]
+        for i in xrange(b.shape[0]):
+            b[i] = bb[i]
+        b[0] = (1 - P)*b[nb-1]
+        gg = b[:(N-1)*(spklength - 1) + 1:(spklength - 1)]
+        a = 0.
+        for k in xrange(gg.shape[0]):
+            a += p[k]*gg[k]
+        b[0] += a 
+        for i in xrange(spklength-1,1+(N-1)*(spklength-1),
+                        spklength-1):
+            b[i] = b[nb-1]
+        s = 0.
+        for k in xrange(b.shape[0]):
+            s += b[k]
+        b[:] = b[:] / (s + tiny)
+        g[:, t] = g[:, t] * b + tiny
+
+def backward2(data, W, g, spklength, 
             winlength, c, q, N, p, b,f):
     tiny = np.exp(-700)
     P = p.sum()
     for t in xrange(winlength - 2, -1, -1):
         f[:] = W - data[t + 1]
-        f[:] = np.exp(-0.5*(f*f*c)) + tiny
-        b = b*f + tiny
-        b[q] = b
+        f[:] = np.exp(-0.5*f*f*c) + tiny
+        b[:] = b*f + tiny
+        for i in xrange(b.shape[0]):
+            b[q[i]] = b[i]
         b[0] = (1 - P)*b[-1]
         gg = b[:(N-1)*(spklength - 1) + 1:(spklength - 1)]
         a = 0.
         for k in xrange(gg.shape[0]):
             a += p[k]*gg[k]
-        b[0] += a
+        b[0] += a 
         b[(spklength - 1):1 + (N - 1)*(spklength - 1):(spklength - 1)] = b[-1]
-        a = 0.
+        s = 0.
         for k in xrange(b.shape[0]):
-            a += b[k]
-        b = b / (a + tiny)
+            s += b[k]
+        b[:] = b / (s + tiny)
         g[:, t] = g[:, t] * b + tiny
 
 def learn(data,spkform=None,iterations=10,cinv=None,p=None,splitp=None,dosplit=True,states=60,
@@ -117,6 +174,7 @@ def learn(data,spkform=None,iterations=10,cinv=None,p=None,splitp=None,dosplit=T
     nchunks = int(np.ceil(1.0*data.shape[0]/chunksize))
     chunks = np.append(np.arange(0,data.shape[0],chunksize),[data.shape[0]])
     chunksizes = np.diff(chunks).astype(np.int)
+    packed_chunksizes = np.zeros((nchunks,),dtype=np.int)
     nchunks = len(chunksizes)
     dtf = 0
     dtb = 0
@@ -126,108 +184,141 @@ def learn(data,spkform=None,iterations=10,cinv=None,p=None,splitp=None,dosplit=T
         sys.stdout.flush()
         W = W.flatten()
         fid = tempfile.TemporaryFile(dir=tempPath)
+        files = ['']*nchunks
         p = p_reset
         g = np.zeros((N*(spklength-1)+1,chunksize))
-        b = np.zeros((g.shape[0],))
+        b = np.zeros((g.shape[0], ))
         f = np.zeros(W.shape)
-        g[0,0] = 1
+        g[0, 0] = 1
         b[0] = 1
         #compute probabilities  
         #note that we are looping over number of states here; the most we are
         #creating is 2Xdata.nbytes
         #forward
-        print "\tRunning forward algorithm..."
-        sys.stdout.flush()
-        for i in xrange(nchunks):
-            print "\t\tAnalyzing chunk %d of %d" % (i+1, nchunks) 
-            t1 = time.time()
-            forward(data[chunks[i]:chunks[i+1]],W,g,spklength,chunksizes[i],cinv,
-                   q,N,p,f)
-            t2 = time.time()
-            #compute average duration
-            dtf = (dtf*i + (t2 - t1))/(i + 1)
-            print "That took %.2f seconds. ETTG: %.2f" %(t2-t1,
-                                                         dtf*(nchunks-(i + 1)))
-            g = g[:, :chunksizes[i]]
-            kk = 0
-            while kk < 100:
-                #try saving the file
+        try:
+            print "\tRunning forward algorithm..."
+            sys.stdout.flush()
+            for i in xrange(nchunks):
+                print "\t\tAnalyzing chunk %d of %d" % (i+1, nchunks) 
+                np.seterr(under='warn')
+                t1 = time.time()
+                fid = tempfile.NamedTemporaryFile(dir=tempPath,delete=False)
+                files[i] = fid.name
                 try:
-                    g.tofile(fid)
-                except ValueError:
-                    """
-                    for some reason, sometimes we get a value error here. If that
-                    happens, just report the exception and let sge know an error
-                    occured
-                    """
-                    kk += 1
-                    time.sleep(10)
-                else:
-                    #if no exception occurred, we mangaed to save the file, so
-                    #break out of the loop
-                    break
-                    #traceback.print_exc(file=sys.stdout)
-                    #if __name__ == '__main__':
-                        #only exit if we are running this as a script
-                   #     sys.exit(99)
-            if kk == 100:
-                #if we reach here it means that we could not save the file
-                if __name__ == '__main__':
-                    print """Could not save temporary file, most likely because of
-                    lack of disk space"""
-                    sys.exit(99)
-                else:
-                    #raise an IO error
-                    raise IOError('Could not save temporary file')
-            g[:, 0] = g[:, -1]
+                    forward(data[chunks[i]:chunks[i+1]],W,g,spklength,chunksizes[i],cinv,
+                           q,N,p,f)
+                except FloatingPointError:
+                    #if we encounter numerical problems, run again with the a
+                    #larger covariance matrix
+                    cinv /= 200
+                    forward(data[chunks[i]:chunks[i+1]],W,g,spklength,chunksizes[i],cinv,
+                           q,N,p,f)
 
-        #backward
-        print "\tRunning backward algorithm..."
-        sys.stdout.flush()
-        G = np.zeros((g.shape[0], )) 
-        for i in xrange(nchunks - 1, -1, -1):
-            print "\t\tAnalyzing chunk %d of %d" % (i + 1, nchunks) 
+
+                t2 = time.time()
+                #compute average duration
+                dtf = (dtf*i + (t2 - t1))/(i + 1)
+                print "That took %.2f seconds. ETTG: %.2f" %(t2-t1,
+                                                             dtf*(nchunks-(i + 1)))
+                g = g[:, :chunksizes[i]]
+                gp = blosc.pack_array(g)
+                packed_chunksizes[i] = len(gp)
+                kk = 0
+                while kk < 100:
+                    #try saving the file
+                    try:
+                        fid.write(gp)
+                        fid.close()
+                    except ValueError:
+                        """
+                        for some reason, sometimes we get a value error here. If that
+                        happens, just report the exception and let sge know an error
+                        occured
+                        """
+                        kk += 1
+                        time.sleep(10)
+                    else:
+                        #if no exception occurred, we mangaed to save the file, so
+                        #break out of the loop
+                        break
+                        #traceback.print_exc(file=sys.stdout)
+                        #if __name__ == '__main__':
+                            #only exit if we are running this as a script
+                       #     sys.exit(99)
+                if kk == 100:
+                    #if we reach here it means that we could not save the file
+                    if __name__ == '__main__':
+                        print """Could not save temporary file, most likely because of
+                        lack of disk space"""
+                        sys.exit(99)
+                    else:
+                        #raise an IO error
+                        raise IOError('Could not save temporary file')
+                g[:, 0] = g[:, -1]
+
+            #backward
+            print "\tRunning backward algorithm..."
+            sys.stdout.flush()
+            G = np.zeros((g.shape[0], )) 
+            for i in xrange(nchunks - 1, -1, -1):
+                print "\t\tAnalyzing chunk %d of %d" % (i + 1, nchunks) 
+                t1 = time.time()
+                fid = open(files[i],'r')
+                g = blosc.unpack_array(fid.read(packed_chunksizes[i]))
+                fid.close()
+                g = g.reshape(N*(spklength - 1) + 1, chunksizes[i])
+                backward(data[chunks[i]:chunks[i+1]], W, g, spklength,
+                        chunksizes[i], cinv, q, N, p, b, f)
+                t2 = time.time()
+                dtb = (dtb*i + (t2 - t1))/(i + 1)
+                print "That took %.2f seconds. ETTG: %.2f" %(t2-t1,
+                                                             dtb*(i))
+                np.seterr(under='warn')
+                g = g / (g.sum(0) + tiny)
+                G += g.sum(1)
+                gp = blosc.pack_array(g)
+                #update the block size
+                packed_chunksizes[i] = len(gp)
+                #rewind the file
+                fid = open(files[i],'w')
+                fid.write(gp)
+                fid.close()
+            
+            #TODO: This stop could be quite memory intensive
+            np.seterr(under='warn')
+            W = np.zeros(W.shape)
             t1 = time.time()
-            backward(data[chunks[i]:chunks[i+1]], W, g, spklength,
-                    chunksizes[i], cinv, q, N, p, b, f)
+            print "Constructing from file chunks..."
+            for i in xrange(nchunks):
+                fid = open(files[i],'r')
+                g = blosc.unpack_array(fid.read())
+                fid.close()
+                g = g.reshape(N*(spklength - 1) + 1, chunksizes[i])
+                W += np.dot(g,data[chunks[i]:chunks[i+1], 0]).flatten()
             t2 = time.time()
-            dtb = (dtb*i + (t2 - t1))/(i + 1)
-            print "That took %.2f seconds. ETTG: %.2f" %(t2-t1,
-                                                         dtb*(i))
-            g = g / (g.sum(0) + tiny)
-            G += g.sum(1)
-            #rewind file
-            fid.seek(-(N*(spklength - 1) + 1)*chunksizes[i]*g[0, 0].nbytes, 1)
-            g.tofile(fid)
-
-        #TODO: This stop could be quite memory intensive
-        W = np.zeros(W.shape)
-        fid.seek(0)
-        t1 = time.time()
-        for i in xrange(nchunks):
-            g = np.fromfile(fid,dtype=np.float,
-                            count=(N*(spklength-1)+1)*
-                            chunksizes[i]).reshape(N*(spklength-1)+1, 
-                            chunksizes[i])
-            W += np.dot(data[chunks[i]:chunks[i+1], :].T, g.T).flatten()
-        t2 = time.time()
-        print "Constructing W tok %.2f seconds" % (t2 - t1, )
-        W = W / G[None,:]
-        W[:,0] = 0
-        p = np.zeros((N, ))
-        fid.seek(0)
-        D = np.memmap(tempfile.TemporaryFile(),dtype=np.float,shape=data.shape,mode='w+')
-        t1 = time.time()
-        for i in xrange(nchunks):
-            g = np.fromfile(fid, dtype=np.float,
-                            count=(N*(spklength - 1) + 1)*chunksizes[i])
-            g = g.reshape(N*(spklength-1) + 1, chunksizes[i])
-            p += g[1::(spklength - 1),:].sum(1)
-
-            D[chunks[i]:chunks[i+1], :] = (W[:, :, None]*g[None, :, :]).sum(1).T
-        t2 = time.time()
-        print "Constructing D tok %.2f seconds" % (t2 - t1, )
-        fid.close() 
+            print "Constructing W tok %.2f seconds" % (t2 - t1, )
+            W = W / G
+            W[0] = 0
+            p = np.zeros((N, ))
+            D = np.memmap(tempfile.TemporaryFile(),dtype=np.float,shape=data.shape,mode='w+')
+            t1 = time.time()
+            print "Constructing D from file chunks..."
+            for i in xrange(nchunks):
+                fid = open(files[i],'r')
+                g = blosc.unpack_array(fid.read())
+                fid.close()
+                g = g.reshape(N*(spklength-1) + 1, chunksizes[i])
+                p += g[1::(spklength - 1),:].sum(1)
+                try:
+                    D[chunks[i]:chunks[i+1], 0] = (W[:, None]*g).sum(0).T
+                except FloatingPointError:
+                    pass
+            t2 = time.time()
+            print "Constructing D tok %.2f seconds" % (t2 - t1, )
+        finally:
+            for f in files:
+                if os.path.isfile(f):
+                    os.unlink(f)
         p = p / winlength
         if data.shape[1] > 1:
             cinv = np.linalg.pinv(np.cov((data-D).T))
@@ -237,9 +328,9 @@ def learn(data,spkform=None,iterations=10,cinv=None,p=None,splitp=None,dosplit=T
         del D
         maxamp = np.zeros((len(spkform),))
         for j in xrange(len(spkform)):
-            spkform[j] = np.concatenate((W[:,0][:,None],
-                                         W[:,j*(spklength-1)+1:(j+1)*(spklength-1)+1]),
-                                        axis=1)
+            spkform[j] = np.concatenate(([W[0]],
+                                         W[j*(spklength-1)+1:(j+1)*(spklength-1)+1]),
+                                        axis=0)
             maxamp[j] = (spkform[j]*(np.dot(cinv, spkform[j]))).sum(0).max(0)
 
         nspikes = p*winlength
@@ -281,9 +372,9 @@ def learn(data,spkform=None,iterations=10,cinv=None,p=None,splitp=None,dosplit=T
     #del g,fit 
     del g 
     for j in xrange(len(spkform)):
-        spkform[j,:,:-1] = np.concatenate((W[:,1][:,None], 
-                                           W[:,j*(spklength-1)+1:(j+1)*(spklength-1)])
-                                          ,axis=1)
+        spkform[j,:,:-1] = np.concatenate(([W[1]], 
+                                           W[j*(spklength-1)+1:(j+1)*(spklength-1)])
+                                          ,axis=0)
     
     return data,spkform,p,cinv
         
