48cef18d5aea07c0376b9a4b469b39b397f61298
Roger Herikstad <roger.herikstad@gmail.com>
2018-01-18 23:31:15 +0800

    Changes to hmmshort.cmd

--- hmmsort.cmd
+++ hmmsort.cmd
@@ -1,12 +1,17 @@
 universe = vanilla
-executable = /home/roger/numba/bin/hmm_learn.py
-arguments = "--sourceFile $(dir)/highpass.mat --iterations 3 --version 3 --chunkSize 100000"
-transfer_executable = false
-output	=	$(dir)/hmmsort.out
-error	=	$(dir)/hmmsort.err
-log		=	$(dir)/hmmsort.log
+executable = /Volumes/DataX/Users/grogee/Documents/programming/hmmsort/dist/hmm_learn
+sortdir = hmmsort
+arguments = "--sourceFile $(dir)/$(fname) --iterations 3 --version 3 --chunkSize 100000"
+should_transfer_files = YES
+transfer_executable = true
+when_to_transfer_output = ON_EXIT
+transfer_input_files = $(dir)/$(fname)
+output	=	$(dir)/hmmsort/hmmsort.out
+error	=	$(dir)/hmmsort/hmmsort.err
+log		=	$(dir)/hmmsort/hmmsort.log
 getenv  = true
 request_memory=6G
 request_disk = 15G
+RunAsOwner = false
 
 queue 1 
