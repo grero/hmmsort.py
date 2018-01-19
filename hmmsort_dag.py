941a4585c01bbe4015d413dccadf60736cfab0f7
Roger Herikstad <roger.herikstad@gmail.com>
2018-01-19 13:43:31 +0800

    Applies changes to make the DAG script more robust

--- hmmsort_dag.py
+++ hmmsort_dag.py
@@ -22,20 +22,34 @@ if __name__ == '__main__':
     thislevel = level(os.getcwd())
     # get all highpass datafiles
     levelidx = levels.index(thislevel)
-    # construct a pattern for finding all highpass files below this level
-    bb = os.sep.join([levels[i]+"*" for i in xrange(levelidx+1,len(levels))])
-    bb = os.sep.join([bb] + ["highpass.mat"])
+    if levelidx == len(levels)-1:
+        bb = "." 
+        ch = 1
+    else:
+        # construct a pattern for finding all highpass files below this level
+        bb = os.sep.join([levels[i]+"*" for i in xrange(levelidx+1,len(levels))])
+        ch = None
+    bb = os.sep.join([bb] + ["*highpass.mat"])
     files = glob.glob(bb)
     
     with open("hmmsort.dag","w") as fid:
         for f in files:
             pp = f.split(os.sep)  # get the channel
+            fn = pp[-1]
             dd = os.sep.join(pp[:-1])
-            ch = int(filter(str.isdigit, pp[-2]))
-            fid.write('JOB hmmlearn_%d hmmsort.cmd\n' % (ch, ))
-            fid.write('VARS hmmlearn_%d fname=%s\n' %(ch, f))
-            fid.write('JOB hmmdecode_%d hmmdecode.cmd\n' % (ch, ))
-            fid.write('VARS hmmdecode_%d fname=%s\n' %(ch, f))
+            # make sure that the output dir exists
+            outdir = os.path.isdir(os.sep.join(dd, "hmmsort"))
+            if not os.path.isdir(outdir):
+                os.mkdir(outdir)
+            if ch is None:
+                ch = int(filter(str.isdigit, pp[-2]))
+            fid.write('JOB hmmlearn_%d %s/hmmsort.cmd DIR %s\n' % (ch, execroot,dd))
+            fid.write('VARS hmmlearn_%d fname="%s"\n' %(ch, fn))
+            fid.write('VARS hmmlearn_%d execroot="%s"\n' %(ch, execroot))
+            fid.write('JOB hmmdecode_%d %s/hmmdecode.cmd DIR %s\n' % (ch, execroot, dd))
+            fid.write('VARS hmmdecode_%d fname="%s"\n' %(ch, fn))
+            fid.write('VARS hmmdecode_%d outfile="hmmsort/spike_templates.hdf5"\n' %(ch, ))
+            fid.write('VARS hmmdecode_%d execroot="%s"\n' %(ch, execroot))
             fid.write('PARENT hmmlearn_%d CHILD hmmdecode_%d\n' % (ch, ch))
             fid.write('\n')
 
