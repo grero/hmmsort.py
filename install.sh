6c66171203070fb1a9a090bce1966e9b9929e3d8
Roger Herikstad <roger.herikstad@gmail.com>
2018-01-23 14:44:05 +0800

    Adds installation script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TARGET=/opt/data2/Software/HTCondor
+
+cp hmmsort_dag.py $TARGET/
+cp hmm_learn $TARGET/
+rm -Rf $TARGET/hmm_decode.app
+cp -r hmm_decode.app $TARGET/
+cp run_hmm_decode.sh $TARGET/
+cp hmmsort.cmd $TARGET/
+cp hmmdecode.cmd $TARGET/
