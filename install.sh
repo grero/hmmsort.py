00140512b7fc12522709824c40b89d453d8b6d01
Roger Herikstad <roger.herikstad@gmail.com>
2018-01-24 09:23:49 +0800

    Fixes hmm_learn executable location

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 TARGET=/opt/data2/Software/HTCondor
 
 cp hmmsort_dag.py $TARGET/
-cp hmm_learn $TARGET/
+cp dist/hmm_learn $TARGET/
 rm -Rf $TARGET/hmm_decode.app
 cp -r hmm_decode.app $TARGET/
 cp run_hmm_decode.sh $TARGET/
