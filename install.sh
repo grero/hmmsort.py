dce4eafd7bed8193007664793501185ab33d4ffc
Roger Herikstad <roger.herikstad@gmail.com>
2018-01-26 17:13:34 +0800

    Updates install script

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ cp dist/hmm_learn $TARGET/
 rm -Rf $TARGET/hmm_decode.app
 cp -r hmm_decode.app $TARGET/
 cp run_hmm_decode.sh $TARGET/
+cp hmm_decode.sh $TARGET/
 cp hmmsort.cmd $TARGET/
 cp hmmdecode.cmd $TARGET/
+cp hmmsort_pbs.py $TARGET/
