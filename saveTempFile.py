e94ae485119a5f1934fe7de3d3a5c12aae7ad13d
akihumii <a0126587@u.nus.edu>
2018-06-22 16:01:36 +0800

    got a trial python scripts used for saving tempfile in some place

--- saveTempFile.py
+++ saveTempFile.py
@@ -5,6 +5,17 @@ import tempfile
 import time
 import os
 
+# tempPath = os.path.join('C:','Users','a0126587','Desktop','tempfile')
+ps = os.sep
+
+tempPath = 'C:'+ps+'Users'+ps+'a0126587'+ps+'Desktop'+ps+'tempfile'
+
+# f = tempfile.NamedTemporaryFile(dir=tempPath,delete=False)
+# f.write(b'Hello world!')
+# print(f.name)
+# f.close() # file is not immediately deleted because we
+#           # used delete=False
+
 def shortenCWD():
     """
     Creates a shortened name for the current working directory
@@ -24,21 +35,26 @@ if __name__ == '__main__':
 
     iteration = 10
 
-    scwd = 'a'
-    tempPath = os.path.join('C:','Users','a0126587','Desktop','tempfile')
-
     scwd = shortenCWD()
+    size = 100000
+    print_strs = 'Hello World'
+    n = 10000
+    # try_array = np.array([[1,2],[3,4]])
+    print_strs = ''.join(char*n for char in print_strs)
 
     for bw in range(iteration):
-        sys.stdout.flush()
+        # sys.stdout.flush()
         # tempPath = '~/hpctmp2'
-        fid = tempfile.NamedTemporaryFile(dir=tempPath,delete=False,prefix=scwd)
+        # fid = tempfile.NamedTemporaryFile(dir=tempPath, delete=False, prefix=scwd)
+        fid = tempfile.NamedTemporaryFile(dir=tempPath, delete=False, prefix=scwd)
 
         kk = 0
         while kk < 100:
             try:
-                fid.write('Hi')
-                print('file' + str(i) + 'is written...')
+                for i in range(1,10):
+                    fid.write(bytes(print_strs, 'utf-8'))
+                    time.sleep(1)
+                    print('file' + str(bw) + str(i) + 'is written...')
             except ValueError:
                 kk += 1
                 time.sleep(10)
