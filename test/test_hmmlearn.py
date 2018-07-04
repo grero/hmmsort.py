31a6e573050fa355d3535362ede3160715914de6
roger <roger.herikstad@gmail.com>
2018-07-04 17:34:38 +0800

    Fixes an issue in the test such that estimated templates are compared with their best match in the ground truth set

--- test/test_hmmlearn.py
+++ test/test_hmmlearn.py
@@ -2,6 +2,7 @@ import hmmsort
 import hmmsort.hmm_learn
 import h5py
 import os
+import numpy as np
 
 #download link https://cortex.nus.edu.sg:6949/sharing/cdrEAsNik
 #https://cortex.nus.edu.sg:6949/sharing/Q4YXxbofM
@@ -11,11 +12,23 @@ def test_spikeforms():
     spikeForms, cinv = hmmsort.hmm_learn.learnTemplatesFromFile("highpass.mat", 1,chunksize=80000, version=3, debug=True, iterations =3, states=60,max_size=800000, min_snr=4.0,maxp=12.0)
 
     ff = h5py.File("spike_templates_master.hdf5","r")
-    mSpikeForms = ff["spikeForms"]
+    mSpikeForms = ff["spikeForms"][:]
     sSpikeForms = spikeForms["second_learning"]["spikeForms"]
     assert mSpikeForms.shape == sSpikeForms.shape
-    d = sSpikeForms - mSpikeForms
-    d2 = d*d
-    assert d2.sum() < 1e-3
+    #figure out the relative shift
+    min_pts_m = mSpikeForms[:].argmin(2).flatten()
+    min_pts_s = sSpikeForms.argmin(2).flatten()
+    nstates = mSpikeForms.shape[-1]
+    ss = np.zeros((mSpikeForms.shape[0],))
+    for i in xrange(len(ss)):
+        #find the smallest shift
+        ii = np.argmin([np.abs(sSpikeForms[i,0,min_pts_s[i]] - mSpikeForms[j,0,min_pts_m[j]]) for j in xrange(mSpikeForms.shape[0])])
+        ds = min_pts_s[i] - min_pts_m[ii]
+        d = sSpikeForms[i,0, ds:] - mSpikeForms[ii, 0, :nstates-ds]
+        d2 = d*d
+        ee = (sSpikeForms[i,0,:]**2).sum()
+        ss[i] = d2.sum()/ee
+    #accept the result if the deviation is less than 5% of the original spike template energy"
+    assert (ss < 0.05).all() 
     os.chdir(pwd)
 
