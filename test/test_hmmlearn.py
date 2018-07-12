6435a7515d6f543cfab6750ad9c3d6d9a1b05048
roger <roger.herikstad@gmail.com>
2018-07-12 09:14:26 +0800

    Uses rseed in test to ensure reproducibility

--- test/test_hmmlearn.py
+++ test/test_hmmlearn.py
@@ -9,9 +9,12 @@ import numpy as np
 def test_spikeforms():
     pwd = os.getcwd()
     os.chdir("test")
-    spikeForms, cinv = hmmsort.hmm_learn.learnTemplatesFromFile("highpass.mat", 1,chunksize=80000, version=3, debug=True, iterations =3, states=60,max_size=800000, min_snr=4.0,maxp=12.0)
+    rseed = 1234
+    spikeForms, cinv = hmmsort.hmm_learn.learnTemplatesFromFile("highpass.mat", 1,chunksize=80000, version=3, debug=True,
+                                                                iterations=3, states=60, max_size=800000, min_snr=4.0,
+                                                                maxp=12.0, rseed=rseed)
 
-    ff = h5py.File("spike_templates_master.hdf5","r")
+    ff = h5py.File("spike_templates_master.hdf5", "r")
     mSpikeForms = ff["spikeForms"][:]
     sSpikeForms = spikeForms["second_learning"]["spikeForms"]
     assert mSpikeForms.shape == sSpikeForms.shape
