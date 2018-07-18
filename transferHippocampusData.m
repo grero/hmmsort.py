24964da53904d3e2812eeee08f4c09fc1961f1ee
akihumii <a0126587@u.nus.edu>
2018-07-18 17:30:02 +0800

    added a function to transfer all the current folder into hippocampus cloud with the direcotry inside picasso and within the folder of the name of that day ended with 'temp'

---
+++ transferHippocampusData.m
@@ -0,0 +1,29 @@
+function [] = transferHippocampusData()
+%TRANSFERHIPPOCAMPUSDATA To be called in the hmmsort_pbs.py to be sumitted
+%as a job onto PBS queue.
+% 
+%   Detailed explanation goes here
+
+cwd = pwd;
+
+indexStarting = strfind(cwd,'2018');
+
+picassoDir = fullfile(filesep,'volume1','Hippocampus','Data','picasso');
+dayStr = cwd([indexStarting:indexStarting+7,'temp']);
+targetDir = fullfile(picassoDir, dayStr);
+
+cd ..
+
+system('tar -czf data.tar.gz cwd');
+
+system(['scp -P 8398 testTarTar.tar.gz hippocampus@cortex.nus.edu.sg:',targetDir]);
+
+system('ssh -p 8398 hippocampus@cortex.nus.edu.sg');
+
+cd(targetDir)
+
+system('tar -xzf data.tar.gz');
+
+
+end
+
