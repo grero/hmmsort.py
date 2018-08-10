25fa0e83365b15c1fcd849296a2f1ea2169198bb
akihumii <a0126587@u.nus.edu>
2018-08-10 10:57:09 +0800

    added while loop to loop the failed ssh jobs

--- transferHippocampusData.m
+++ transferHippocampusData.m
@@ -1,13 +1,13 @@
 function [] = transferHippocampusData()
 %TRANSFERHIPPOCAMPUSDATA To be called in the hmmsort_pbs.py to be sumitted
 %as a job onto PBS queue.
-% 
+%
 %   Detailed explanation goes here
 
 cwd = pwd;
 
 dataName = strrep(cwd,filesep,'_');
-fileName = [dataName,'.tar.gz']; % tar.gz name 
+fileName = [dataName,'.tar.gz']; % tar.gz name
 
 indexDay = strfind(cwd,'2018');
 
@@ -23,24 +23,72 @@ cd ..
 system(['tar -cvzf ',fileName,' ',cwd]);
 disp(' ');
 
-system(['scp -P 8398 ',fileName,' hippocampus@cortex.nus.edu.sg:~/']);
-disp(['Secured copied ',fileName,' to home directory of hippocampus ...']);
+[flag, count] = resetFlags;
+while ~flag && count < 100
+    try
+        system(['scp -P 8398 ',fileName,' hippocampus@cortex.nus.edu.sg:~/']);
+        disp(['Secured copied ',fileName,' to home directory of hippocampus ...']);
+        flag = 1;
+    catch
+        disp('Retrying scp tar file to home directory of hippocampus...')
+        pause(10)
+    end
+end
 disp(' ');
 
-system([sshHippocampus,' mkdir -p ', targetDir]);
-disp(['Made a directory ',targetDir,' ...']);
+[flag, count] = resetFlags;
+while ~flag && count < 100
+    try
+        system([sshHippocampus,' mkdir -p ', targetDir]);
+        disp(['Made a directory ',targetDir,' ...']);
+        flag = 1;
+    catch
+        disp('Retrying making temporary directory...')
+        pause(10)
+    end
+end
 disp(' ');
 
-system([sshHippocampus,' mv -v ',fileName,' ',targetDir]);
+flag = 0;
+count = 1;
+while ~flag && count < 100
+    try
+        system([sshHippocampus,' mv -v ',fileName,' ',targetDir]);
+        flag = 1;
+    catch
+        disp('Retrying moving tar file...')
+    end
+end
 disp(' ');
 
-system([sshHippocampus,' tar -xvzf ',fullfile(targetDir,fileName),' -C ',targetDir]);
+[flag, count] = resetFlags;
+while ~flag && count < 100
+    try
+        system([sshHippocampus,' tar -xvzf ',fullfile(targetDir,fileName),' -C ',targetDir]);
+        flag = 1;
+    catch
+        disp('Retrying extracting tar file...')
+    end
+end
 disp(' ')
 
-system([sshHippocampus, ' find ',targetDir,' -name ',dayStr,' | while IFS= read file; do ',sshHippocampus,' cp -vrRup $file ',picassoDir,'; done']);
+[flag, count] = resetFlags;
+while ~flag && count < 100
+    try
+        system([sshHippocampus, ' find ',targetDir,' -name ',dayStr,' | while IFS= read file; do ',sshHippocampus,' cp -vrRup $file ',picassoDir,'; done']);
+        flag = 1;
+    catch
+        disp('Retrying moving files to proper places...')
+    end
+end
 disp(' ');
 
 %system([sshHippocampus,' rm -v ',fullfile(targetDir,dataName)]);
 
+end
+
+function [flag, count] = resetFlags()
+flag = 0;
+count = 1;
 end
 
