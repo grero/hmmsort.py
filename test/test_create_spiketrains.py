4bd050d3b3871d187d4ffe8005b72daf77503304
roger <roger.herikstad@gmail.com>
2018-09-19 20:48:22 +0800

    Adds test for resulting spike trains

--- test/test_create_spiketrains.py
+++ test/test_create_spiketrains.py
@@ -5,6 +5,8 @@ import os
 import pytest
 import urllib2
 import tempfile
+import hashlib
+import scipy.io as mio
 
 def download_file(fname):
     _file = urllib2.urlopen("http://cortex.nus.edu.sg/testdata/array01/channel001/%s" % (fname,))
@@ -29,6 +31,13 @@ def test_pick_lines(qtbot):
     event = matplotlib.backend_bases.PickEvent("A pick", window.figure.canvas, [],window.figure.axes[0].lines[1]) 
     window.pick_event(event)
     window.save_spiketrains(notify=False) 
+    #check that we got what we expected
+    q1 =  mio.loadmat("cell01/spiketrain.mat")
+    hh1 = hashlib.sha1(q1["timestamps"].tostring()).hexdigest()
+    assert hh1 == '8093ffc2459eb613bcc10f65e34b0c47568e22ae'
+    q2 =  mio.loadmat("cell02/spiketrain.mat")
+    hh2 = hashlib.sha1(q2["timestamps"].tostring()).hexdigest()
+    assert hh2 == '03de5a1a19919c3ede7030d760aebcf671379c7f'
     os.unlink("hmmsort.mat")
     os.unlink("spike_templates.hdf5")
     os.unlink("cell01/spiketrain.mat")
