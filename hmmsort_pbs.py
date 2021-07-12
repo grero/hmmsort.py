2f688af6843d9e80ed273d6bd2ebcdd8dcc2eb33
Roger Herikstad <lsihr@nus.edu.sg>
2021-07-12 16:20:42 +0800

    Combine template finding and decoding and submit jobs via bash at the end

--- hmmsort_pbs.py
+++ hmmsort_pbs.py
@@ -44,9 +44,11 @@ if __name__ == '__main__':
     # save current directory path since we will be changing directories later
     currentdir = os.getcwd();
     execdir,py = os.path.split(sys.executable)
+    master_file = open("sorting.pbs","w")
+    master_file.write("#!/bin/bash\n")
     for i,f in enumerate(files):
-        fname_learn = "learn_job%.4d.pbs" %(i,)
-        fname_decode = "decode_job%.4d.pbs" %(i,)
+        fname_sort = "hmmsort_%.4d.pbs" %(i,)
+
         pp = f.split(os.sep)
         dd = os.sep.join([currentdir] +  pp[:-1])
         if os.path.isfile(os.sep.join([dd, "hmmsort.mat"])):
@@ -56,38 +58,16 @@ if __name__ == '__main__':
         if os.path.isfile(os.sep.join([dd, "sorting_done"])):
             continue
 
+        master_file.write("cd {}\n".format(dd))
+        master_file.write("qsub {}\n".format(fname_sort))
+        master_file.write("cd {}\n".format(currentdir))
         fn = pp[-1]
         # change directories so that the output and error files will be created
         # in the respective channel directores and will be easier to check on
         # the status of the sorting
         os.chdir(dd)
-        with open(fname_learn,"w") as fo:
+        with open(fname_sort,"w") as fo:
             fo.write("#!/bin/bash\n")
-            fo.write("#PBS -l select=1:ncpus=1:mem=6GB\n")
-            # increased request for CPU hours to make sure even long jobs will be able to complete
-            fo.write("#PBS -l walltime=24:00:00\n")
-            fo.write("np=$( cat  ${PBS_NODEFILE} |wc -l );\n")
-            fo.write("source /etc/profile.d/modules.sh\n")
-            fo.write("module load miniconda\n")
-            fo.write("bash\n")
-            fo.write(". ~/.bashrc\n")
-            fo.write("cd %s\n" %(dd,))
-            fo.write("%s/hmm_learn.py --sourceFile %s --iterations 3 " %(execdir,fn))
-            fo.write("--chunkSize 100000 --outFile hmmsort/spike_templates.hdf5 ")
-            fo.write("--min_snr 4.0 ")
-            # get current username instead of hardcoding username
-            fo.write("--max_size 1000000 --tempPath /hpctmp2/%s/tmp/\n" %(getpass.getuser()))
-
-            if not "--dry-run" in dopts.keys():
-                fii = open("sorting_inprogress","w")
-                fii.close()
-                jobid = subprocess.run(['/opt/pbs/bin/qsub', fname_learn],capture_output=True, shell=False).stdout.strip()
-
-        with open(fname_decode,"w") as fo:
-             # request more memory as some decode jobs were being killed for
-             # exceeding the default 4 GB
-            fo.write("#!/bin/bash\n")
-            fo.write("#PBS -N decode\n")
             if queue == "flexi":
                 # commenting out next line as it does not seem necessary
                 # and because I would like to keep the jobid for hmm_learn on the
@@ -106,25 +86,30 @@ if __name__ == '__main__':
             else:
                 fo.write("#PBS -q short\n")
                 fo.write("#PBS -l select=1:ncpus=1:mem=30GB\n")
-            fo.write("#PBS -l walltime=24:00:00\n")
-            fo.write("#PBS -o {}\n".format(os.path.join(dd, "decode.o")))
-            fo.write("#PBS -e {}\n".format(os.path.join(dd, "decode.e")))
-          # increased request for CPU hours to make sure even long jobs will be able to complete
-            if not "--dry-run" in dopts.keys():
-                fo.write("#PBS -W depend=afterok:%s\n" %(jobid, ))
+                fo.write("#PBS -l walltime=24:00:00\n")
+
+            fo.write("#PBS -o {}\n".format(os.path.join(dd, "hmmsort.o")))
+            fo.write("#PBS -e {}\n".format(os.path.join(dd, "hmmsort.e")))
+
+            fo.write("np=$( cat  ${PBS_NODEFILE} |wc -l );\n")
             fo.write("source /etc/profile.d/modules.sh\n")
             fo.write("module load miniconda\n")
             fo.write("conda activate hmmsort\n")
-            fo.write("np=$( cat  ${PBS_NODEFILE} |wc -l );\n")
             fo.write("bash\n")
             fo.write(". ~/.bashrc\n")
             fo.write("cd %s\n" %(dd,))
+            fo.write("touch sorting_inprogress\n")
+            fo.write("%s/hmm_learn.py --sourceFile %s --iterations 3 " %(execdir,fn))
+            fo.write("--chunkSize 100000 --outFile hmmsort/spike_templates.hdf5 ")
+            fo.write("--min_snr 4.0 ")
+            # get current username instead of hardcoding username
+            fo.write("--max_size 1000000 --tempPath /hpctmp2/%s/tmp/\n" %(getpass.getuser()))
+            fo.write("touch templates_found\n")
             fo.write("%s/run_hmm_decode.sh /app1/common/matlab/R2016a/ SourceFile %s Group 1 " %(execdir,fn))
             fo.write("fileName hmmsort/spike_templates.hdf5 save hdf5 ")
             fo.write("SaveFile hmmsort.mat hdf5path after_noise\n")
             fo.write("rm sorting_inprogress\n")
             fo.write("touch sorting_done\n")
 
-            if not "--dry-run" in dopts.keys():
-                 jobid = subprocess.run(['/opt/pbs/bin/qsub', fname_decode], capture_output=True,shell=False).stdout.strip()
+    master_file.close()
     sys.exit(0)
